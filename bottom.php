9ee212c90f213f8ddda442e65af3c0f97638549a
Pankaj <pkandpal601@gmail.com>
2022-03-04 16:50:11 +0530

    first commit

---
+++ bottom.php
@@ -0,0 +1,64 @@
+<div class="fotter">	
+	 <div class="container">
+		 <div class="fotter-grids">
+			 <div class="col-md-4 fotter-left">
+			 <img src="images/fotter-logo.png" alt="">
+			 <p style="text-align:justify">My name is Pankaj, your tour director for your next tour. We are working as a guide for the last One years with Kumaun and Gharwal groups around the most important places in Uttarakhand. This blog is for us, but sharing it with you, it is also for you, so you can find useful information for your next tour. This is our wish. </p>
+			 </div>
+			 <div class="col-md-4 fotter-middle">
+				 <h3>Questions About Our Tours?</h3>
+				 <div class="footer-list">
+						<ul>
+                        <li style="text-align:justify"><a href="#"><span></span>
+Our Customer Service Department is always happy to answer any questions you might have. Please visit our Contact Page to contact us or call us at 8449519925</a></li></ul> </div>
+						<h3>why makemytrip?</h3>
+                        <div class="footer-list">
+                        <ul>
+
+ <li style="text-align:justify"><a href="#"><span></span>Customer Satisfaction<br/></a></li>
+ <li style="text-align:justify"><a href="#"><span></span>Superior customer service, 24x7 Dedicated helpline and over 5 million delighted customers & still growing.<br/></a></li>
+<li style="text-align:justify"><a href="#"><span></span>Best Deals Guaranteed<br/></a></li>
+<li style="text-align:justify"><a href="#"><span></span>Great experiences at lowest prices guaranteed.<br/></a></li>
+</a></li> </ul>
+			 </div>
+             
+			 </div>
+			 <div class="col-md-4 fotter-right" style="padding-left:70px">
+			 <h3>Why buy from us?</h3><br/>
+              <div class="footer-list">
+			  <ul>
+              <li style="color:#FFF"><a href="#" style="color:#FFF"><span></span> Payment Security</a></li>
+              <li style="color:#FFF"><a href="#" style="color:#FFF"><span></span> Travel Guide</a></li>
+              <li style="color:#FFF"><a href="#" style="color:#FFF"><span></span> Social Responsibility</a></li>
+               </ul> </div></div>
+			 <div class="social-icons">
+			 <a href="#"><span class="facebook"> </span></a>
+			 <a href="#"><span class="twitter"> </span></a>
+			 <a href="#"><span class="googleplus"> </span></a>
+			 <a href="#"><span class="pinterest"> </span></a>
+			 <a href="#"><span class="instagram"> </span></a>
+			 </div>
+			 <div class="clearfix"></div>
+	     </div>
+		 <div class="clearfix"></div>
+	 </div>
+</div>
+</div>  
+<div class="copyright text-right">
+<p style="padding-right:100px">Designed by:PANKAJ KANDPAL</p>
+</div>
+  <script src="js/jquery.scrollTo.js"></script>
+	<script src="js/jquery.nav.js"></script>
+	<script type="text/javascript">
+	$(document).ready(function() {
+		$('#nav').onePageNav({
+			begin: function() {
+			console.log('start')
+			},
+			end: function() {
+			console.log('stop')
+			}
+		});
+	});
+	</script>
+  
